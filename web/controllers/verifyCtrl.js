c05044e8143aa189ea9fea386e011ce895328cce
Curtis Merrell <Curtis Merrell>
2016-02-09 16:42:00 -0700

    minor bug

--- web/controllers/verifyCtrl.js
+++ web/controllers/verifyCtrl.js
@@ -1,21 +1,8 @@
-var app = angular.module('squareApp');
+var app = angular.module('squareVerifyApp');
 
-app.controller('verifyCtrl', function($scope, $routeParams) {
-	var code = $routeParams["code"];
-	$.ajax({
-		url: "https://foursquare.com/oauth2/access_token"
-			+ "?client_id=3ANHQ4B311FDPQUSHWQDEVRSQQVDCDD5HBCVPHBEMMLETFTA"
-			+ "&client_secret=RI40Z31FC5AKE50WIIUY5N2UMAV0ETOPJHR3OPVXRJVUS3HH"
-			+ "&grant_type=authorization_code"
-			+ "&redirect_uri=http://54.201.68.154:3000/#/verify"
-			+ "&code=" + code,
-		headers: {
-			"Access-Control-Allow-Origin" : "*"
-		}
-	})
-	.done(function(token){
-		var username = localStorage.currentUser.username;
-		localStorage[username].accessToken = token.access_token;
-		$scope.verificationMessage = "Successfully validated with token " + access_token;
-	});
+app.controller('verifyCtrl', function($scope, $location) {
+	var token = $location.hash().split('=')[1];
+	var username = localStorage.currentUser;
+	localStorage[username].accessToken = token;
+	$scope.verificationMessage = "verified with token: " + token;
 });
