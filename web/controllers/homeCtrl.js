852d8465d6e8d7dde0e29c629c3bb1fc87051268
Aaron Monson <aaron@lmonson.com>
2016-02-09 15:12:07 -0700

    doin control stuffs

--- web/controllers/homeCtrl.js
+++ web/controllers/homeCtrl.js
@@ -1,5 +1,9 @@
 var app = angular.module('squareApp');
 
 app.controller('homeCtrl', function($scope) {
-
+	$scope.loggedIn = false;
+	if (document.cookie) {
+		$scope.username = localStorage.username;
+		$scope.loggedIn = true;
+	}
 });
--- web/controllers/loginCtrl.js
+++ web/controllers/loginCtrl.js
@@ -1,5 +1,19 @@
 var app = angular.module('squareApp');
 
 app.controller('loginCtrl', function($scope) {
+	eraseCookie("username");
+	localStorage[localStorage.username]["active"] = false;
+	delete localStorage.username;
 	$scope.loggedIn = false;
+	$scope.login = function(username) {
+		createCookie("username", username, 1);
+		localStorage.username = username;
+		if (localStorage[username]) {
+			localStorage[username] = localStorage[username] || {active:true};
+		}
+		else {
+			localStorage[username]["active"] = true;
+		}
+		window.location = "#home";
+	}
 });
