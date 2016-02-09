ed6b4c320d4cc0e3b6727f2abbfe655f8a442350
Curtis Merrell <Curtis Merrell>
2016-02-09 11:10:20 -0700

    got rid of old references. Made starting controllers

---
+++ web/controllers/homeCtrl.js
@@ -0,0 +1,5 @@
+var app = angular.module('squareApp');
+
+app.controller('homeCtrl', function($scope) {
+
+});
---
+++ web/controllers/loginCtrl.js
@@ -0,0 +1,5 @@
+var app = angular.module('squareApp');
+
+app.controller('loginCtrl', function($scope) {
+	$scope.loggedIn = false;
+});
