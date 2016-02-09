57c006c3ecc3c9649b0009fc4b0678b00e6e5993
Curtis Merrell <Curtis Merrell>
2016-02-09 13:02:41 -0700

    updating curt branch

--- web/controllers/homeCtrl.js
+++ web/controllers/homeCtrl.js
@@ -1,5 +1,10 @@
 var app = angular.module('squareApp');
 
 app.controller('homeCtrl', function($scope) {
-
+	$scope.loggedIn = function() {
+		if(document.cookie)
+			return true;
+		else
+			return false;
+	}
 });
--- web/controllers/loginCtrl.js
+++ web/controllers/loginCtrl.js
@@ -1,5 +1,29 @@
 var app = angular.module('squareApp');
 
 app.controller('loginCtrl', function($scope) {
-	$scope.loggedIn = false;
+	$scope.loginFailMessage = "";
+	
+	$scope.login = function(username) {
+		if(localStorage[username]) {
+			$scope.loginFailMessage = "";
+			createCookie("username", username, 1);
+			window.location = "#home";
+		}
+		else {
+			$scope.loginFailMessage = "User not found."
+		}
+	};
+	
+	$scope.register = function(username) {
+		if(!localStorage[username]) {
+			$scope.loginFailMessage = "";
+
+			localStorage[username] = username;
+			createCookie("username", username, 1);
+			window.location = "#home";
+		}
+		else {
+			$scope.loginFailMessage = "User already exists."
+		}
+	}
 });
