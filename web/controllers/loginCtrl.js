9b124c2ae2951ed8b6d437b9ee548f86d8efe19a
Aaron Monson <aaron@lmonson.com>
2016-02-09 15:14:59 -0700

    another change

--- web/controllers/loginCtrl.js
+++ web/controllers/loginCtrl.js
@@ -2,17 +2,13 @@ var app = angular.module('squareApp');
 
 app.controller('loginCtrl', function($scope) {
 	eraseCookie("username");
-	localStorage[localStorage.username]["active"] = false;
 	delete localStorage.username;
 	$scope.loggedIn = false;
 	$scope.login = function(username) {
 		createCookie("username", username, 1);
 		localStorage.username = username;
 		if (localStorage[username]) {
-			localStorage[username] = localStorage[username] || {active:true};
-		}
-		else {
-			localStorage[username]["active"] = true;
+			localStorage[username] = localStorage[username] || 1;
 		}
 		window.location = "#home";
 	}
